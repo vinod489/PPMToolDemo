a197ab0a51778eb5df6f0542f0bbc9136d881cf7
vinod489 <uppu.vinodkumar@gmail.com>
2020-03-28 23:07:09 +0530

    first commit

---
+++ springbootdemo/src/main/java/com/fullstackdemo/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.fullstackdemo.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoApplication.class, args);
+	}
+
+}
