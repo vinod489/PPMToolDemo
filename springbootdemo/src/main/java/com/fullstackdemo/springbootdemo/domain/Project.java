1307478f844a448b3424d2c3886e9a120f9808e5
vinod489 <uppu.vinodkumar@gmail.com>
2020-03-29 23:28:39 +0530

    Added project entity and repository

--- springbootdemo/src/main/java/com/fullstackdemo/springbootdemo/domain/Project.java
+++ springbootdemo/src/main/java/com/fullstackdemo/springbootdemo/domain/Project.java
@@ -1,5 +1,105 @@
 package com.fullstackdemo.springbootdemo.domain;
 
-public class Project {
+import java.time.LocalDate;
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+@Entity
+public class Project {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String projectName;
+	private String projectIdentifier;
+	private String projectDescription;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	private LocalDate createdAt;
+	private LocalDate updatedAt;
+	
+	@PrePersist
+	protected void onCreate() {
+		createdAt = LocalDate.now();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		updatedAt = LocalDate.now();
+	}	
+	
+	public Project() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public String getProjectIdentifier() {
+		return projectIdentifier;
+	}
+
+	public void setProjectIdentifier(String projectIdentifier) {
+		this.projectIdentifier = projectIdentifier;
+	}
+
+	public String getProjectDescription() {
+		return projectDescription;
+	}
+
+	public void setProjectDescription(String projectDescription) {
+		this.projectDescription = projectDescription;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public LocalDate getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDate createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDate getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDate updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
 }
---
+++ springbootdemo/src/main/java/com/fullstackdemo/springbootdemo/repositories/ProjectRepository.java
@@ -0,0 +1,11 @@
+package com.fullstackdemo.springbootdemo.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fullstackdemo.springbootdemo.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+	Iterable<Project> findAllById(Long Id);
+}
