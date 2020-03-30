e73436093e151f5dc04c581296a0ddca1cfd93d8
vinod489 <uppu.vinodkumar@gmail.com>
2020-03-30 22:48:27 +0530

    Added checkd for invalid object

--- springbootdemo/src/main/java/com/fullstackdemo/springbootdemo/web/ProjectController.java
+++ springbootdemo/src/main/java/com/fullstackdemo/springbootdemo/web/ProjectController.java
@@ -1,8 +1,11 @@
 package com.fullstackdemo.springbootdemo.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +22,10 @@ public class ProjectController {
 	private ProjectService projectService;
 	
 	@PostMapping("/")
-	public ResponseEntity<Project> saveProject(@RequestBody Project project) {
+	public ResponseEntity<?> saveProject(@Valid @RequestBody Project project, BindingResult result) {
+		if(result.hasErrors()) {
+			return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST) ;
+		}
 		Project savedProject = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(savedProject, HttpStatus.CREATED);
 	}	
