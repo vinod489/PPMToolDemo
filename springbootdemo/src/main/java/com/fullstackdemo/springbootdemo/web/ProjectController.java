0f7712c673b629e77aba191fb1e82101d02e2d41
vinod489 <uppu.vinodkumar@gmail.com>
2020-03-30 23:04:57 +0530

    Representation of error is changed

--- springbootdemo/src/main/java/com/fullstackdemo/springbootdemo/web/ProjectController.java
+++ springbootdemo/src/main/java/com/fullstackdemo/springbootdemo/web/ProjectController.java
@@ -1,11 +1,16 @@
 package com.fullstackdemo.springbootdemo.web;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +29,12 @@ public class ProjectController {
 	@PostMapping("/")
 	public ResponseEntity<?> saveProject(@Valid @RequestBody Project project, BindingResult result) {
 		if(result.hasErrors()) {
-			return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST) ;
+			List<FieldError> fieldErrors = result.getFieldErrors();
+			Map<String, String> errorMap = new HashMap<>();
+			for(FieldError eachFieldError : fieldErrors) {
+				errorMap.put(eachFieldError.getField(), eachFieldError.getDefaultMessage());
+			}
+			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST) ;
 		}
 		Project savedProject = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(savedProject, HttpStatus.CREATED);
